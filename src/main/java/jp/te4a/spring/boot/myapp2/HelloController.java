a0c75d1c83a61f44b914286ed4a51f95f5033585
te210879 <tejc210879@gmail.com>
2024-05-22 15:16:22 +0900

    初回登録

--- src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp1;
+package jp.te4a.spring.boot.myapp2;
  
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 *
 */
 @RestController
-public class HelloController 
+public class HelloController
 {
-	@RequestMapping("/")
-	public String index() {
-		return "Hello, Spring Boot!";
+	@RequestMapping("/taro")
+	public String taro() {
+	  return "Taro desu!";
 	}
 }
